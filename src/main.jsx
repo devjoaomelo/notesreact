2822f7a54c01bd5d5b3d50293720181f5b74a40a
joão <devjoaomelo@gmail.com>
2022-10-18 17:07:06 -0300

    avatar + signup

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { SignIn } from './pages/SignIn'
+import { Profile } from './pages/Profile'
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import GlobalStyles from './styles/global'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <SignIn />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 )
